17c2dd458902ea153c1741da8a259b50a0fffb22
pnmejia17 <114623502+pnmejia17@users.noreply.github.com>
2023-11-17 16:32:13 -0500

    fixed search url update bug

---
+++ config.js
@@ -0,0 +1,3 @@
+const apiKey = '81ba710a6e774ddc101bffdd3856dabf'
+
+export default apiKey
--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Route, Routes, Navigate } from 'react-router-dom'
-import apiKey from './config'
+import apiKey from '../../config'
 import './App.css'
 import Search from '../components/search'
 import Nav from '../components/nav'
@@ -11,18 +11,22 @@ import NoResults from '../components/noresults'
 
 function App() {
   const [photos, setPhotos] = useState([])
-  const [query, setQuery] = useState('cats')
+  // const [query, setQuery] = useState()
 
-  const fetchData = async (query) => {
+
+  const fetchData = (query = 'cats') => {
     fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
       .then(response => response.json())
       .then(responseData => setPhotos(responseData.photos.photo))
       .catch(error => console.log("Error fetching and parsing data", error))
   }
 
+  // console.log(query)
+
+
   useEffect(() => {
-    fetchData(query)
-  }, [query])
+    fetchData()
+  },[])
 
 
   console.log(photos)
@@ -30,11 +34,11 @@ function App() {
   return (
     <>
       <Search fetchData={fetchData}/>
-      <Nav />
+      <Nav search={fetchData}/>
       <Routes>
         <Route path="/" element={<Navigate to="/cats" />} />
         <Route path="/cats" element={<PhotoList photos={photos} title='cats'/>} />
-        <Route path="/dogs" element={<PhotoList photos={photos} title='dogs'/>} />
+        <Route path="/dogs" element={<PhotoList photos={photos} search={fetchData} title='dogs'/>} />
         <Route path="/computers" element={<PhotoList photos={photos} title='computers'/>} />
         <Route path="/search/:query" element={<PhotoList photos={photos} />} />
         <Route path='*' element={<Navigate to="/404" />}/>
